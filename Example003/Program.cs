4cc838babd2088b294ec0a12da716caca262708b
Aigul Aiupova <aigul.sadreeva@gmail.com>
2022-12-17 00:51:12 +0300

    Add Random

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // числа от 1 до 9 включительно
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
