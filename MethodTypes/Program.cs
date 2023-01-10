b6222c912b1acc0181a44b3d7787a47edadde5f1
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-10 22:57:32 +0300

    Лекция 3

---
+++ MethodTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿// вид 4
+// string Method4(int count, string c)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + c;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "текст");
+// Console.WriteLine(res);
+
+// Использование цикла for
+
+string Method4(int count, string c)
+{
+    
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + c;
+    }
+    return result;
+}
+string res = Method4(10, "текст");
+Console.WriteLine(res);
+
