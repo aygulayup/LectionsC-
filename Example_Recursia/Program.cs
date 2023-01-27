840aea9fdd0ff0171043e038129fe29336d80a9d
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-01-27 23:29:02 +0300

    Рекурсия

--- Example_Recursia/Program.cs
+++ Example_Recursia/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Найти факториал числа, используя РЕкурсию.
 
-int Factorial(int n)
+double Factorial(int n)
 {
 
-    if(n == 1) retern 1; // 1! = 1 0! = 1
+    if(n == 1) return 1; // 1! = 1 0! = 1
     else return n * Factorial(n - 1);
 }
-Console.WriteLine(Factorial(3));
+Console.WriteLine(Factorial(20));
